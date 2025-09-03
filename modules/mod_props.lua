c1cda0ff0401ca6eaec4295cb5c6ed23ec3d48df
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2025-09-04 00:09:31 +0700

    feat: mod page colours (requested by @nh6574)

--- modules/mod_props.lua
+++ modules/mod_props.lua
@@ -1,16 +1,16 @@
 -- steamodded wtf why was i not told about this
 
 SMODS.current_mod.ui_config = {
-    colour = G.C.L_BLACK, -- Main UI box
-    bg_colour = {G.C.GREY[1], G.C.GREY[2], G.C.GREY[3], 0.7}, -- Background
-    back_colour = G.C.ORANGE, -- Back button
-    tab_button_colour = G.C.BOOSTER, -- Tabs buttons
-    outline_colour = G.C.JOKER_GREY, -- Main UI box outline
-    author_colour = G.C.BLUE, -- Author text
-    author_bg_colour = G.C.CLEAR, -- Author box background
-    author_outline_colour = G.C.JOKER_GREY, -- Author box outline
-    collection_bg_colour = {G.C.GREY[1], G.C.GREY[2], G.C.GREY[3], 0.7}, -- Collection background (Defaults to bg_colour)
-    collection_back_colour = G.C.ORANGE, -- Collection background (Defaults to back_colour)
-    collection_outline_colour = G.C.JOKER_GREY, -- Collection background (Defaults to outline_colour)
-    collection_option_cycle_colour = G.C.RED, -- Collection option cycle button
+    colour = G.C.AKYRS_HAIR_DARK, -- Main UI box
+    bg_colour = G.C.AKYRS_AIKOYORI_BG, -- Background
+    back_colour = G.C.AKYRS_AIKOYORI_CARDIGAN, -- Back button
+    tab_button_colour = G.C.AKYRS_AIKOYORI_BOW_SHADE, -- Tabs buttons
+    outline_colour = G.C.AKYRS_AIKOYORI_HAIR, -- Main UI box outline
+    author_colour = G.C.AKYRS_AIKOYORI_HAIR, -- Author text
+    author_bg_colour = G.C.AKYRS_HAIR_DARKER, -- Author box background
+    author_outline_colour = G.C.AKYRS_AIKOYORI_BOW, -- Author box outline
+    collection_bg_colour = G.C.AKYRS_AIKOYORI_BG,
+    collection_back_colour = G.C.AKYRS_AIKOYORI_CARDIGAN, -- Collection background (Defaults to back_colour)
+    collection_outline_colour = G.C.AKYRS_AIKOYORI_HAIR, -- Collection background (Defaults to outline_colour)
+    collection_option_cycle_colour = G.C.AKYRS_AIKOYORI_BOW_SHADE, -- Collection option cycle button
 }
